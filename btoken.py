0ed49db8071f9f428c0b587e8afa6e5d77b8957c
richpl <rich.lampard@gmail.com>
2018-11-17 19:17:11 +0000

    Added support for two character small tokens

--- btoken.py
+++ btoken.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 
-"""
-Class to represent a token for the BASIC
+"""Class to represent a token for the BASIC
 programming language. A token consists of
 three items:
 
@@ -13,49 +12,52 @@ lexeme      Token in string form
 
 class BToken:
 
-        """
-        BToken categories
-        """
-        EOF         = 0   # End of file
-        LET         = 1   # LET keyword
-        LIST        = 2   # LIST command
-        PRINT       = 3   # PRINT command
-        RUN         = 4   # RUN command
-        FOR         = 5   # FOR keyword
-        NEXT        = 6   # NEXT keyword
-        IF          = 7   # IF keyword
-        THEN        = 8   # THEN keyword
-        ELSE        = 9   # ELSE keyword
-        ASSIGNOP    = 10  # '='
-        LEFTPAREN   = 11  # '('
-        RIGHTPAREN  = 12  # ')'
-        PLUS        = 13  # '+'
-        MINUS       = 14  # '-'
-        TIMES       = 15  # '*'
-        DIVIDE      = 16  # '/'
-        NEWLINE     = 17  # End of line
-        NUMBER      = 18  # Real or integer, may be signed
-        NAME        = 19  # Identifier that is not a keyword
-        EXIT        = 20  # Used to quit the interpreter
-        DIM         = 21  # DIM keyword
-        GREATER     = 22  # '>'
-        LESSER      = 23  # '<'
-        STEP        = 24  # STEP keyword
-        GOTO        = 25  # GOTO keyword
-        GOSUB       = 26  # GOSUB keyword
-        INPUT       = 27  # INPUT keyword
-        REM         = 28  # REM keyword
-        RETURN      = 29  # RETURN keyword
-        SAVE        = 30  # SAVE command
-        LOAD        = 31  # LOAD command
-        ERROR       = 99  # None of the above
+        """BToken categories"""
 
-        #category_names = {0: 'EOF', 1: 'LET'}
+        EOF             = 0   # End of file
+        LET             = 1   # LET keyword
+        LIST            = 2   # LIST command
+        PRINT           = 3   # PRINT command
+        RUN             = 4   # RUN command
+        FOR             = 5   # FOR keyword
+        NEXT            = 6   # NEXT keyword
+        IF              = 7   # IF keyword
+        THEN            = 8   # THEN keyword
+        ELSE            = 9   # ELSE keyword
+        ASSIGNOP        = 10  # '='
+        LEFTPAREN       = 11  # '('
+        RIGHTPAREN      = 12  # ')'
+        PLUS            = 13  # '+'
+        MINUS           = 14  # '-'
+        TIMES           = 15  # '*'
+        DIVIDE          = 16  # '/'
+        NEWLINE         = 17  # End of line
+        UNSIGNEDINT     = 18  # Integer
+        NAME            = 19  # Identifier that is not a keyword
+        EXIT            = 20  # Used to quit the interpreter
+        DIM             = 21  # DIM keyword
+        GREATER         = 22  # '>'
+        LESSER          = 23  # '<'
+        STEP            = 24  # STEP keyword
+        GOTO            = 25  # GOTO keyword
+        GOSUB           = 26  # GOSUB keyword
+        INPUT           = 27  # INPUT keyword
+        REM             = 28  # REM keyword
+        RETURN          = 29  # RETURN keyword
+        SAVE            = 30  # SAVE command
+        LOAD            = 31  # LOAD command
+        NOTEQUAL        = 32  # '<>'
+        LESSEQUAL       = 33  # '<='
+        GREATEREQUAL    = 34  # '>='
+        UNSIGNEDFLOAT   = 35  # Floating point number
+        STRING          = 36  # String values
+        ERROR           = 99  # None of the above
 
         smalltokens = {'=': ASSIGNOP, '(': LEFTPAREN, ')': RIGHTPAREN,
                        '+': PLUS, '-': MINUS, '*': TIMES, '/': DIVIDE,
-                       '\n': NEWLINE, '': EOF, '<': LESSER,
-                       '>': GREATER}
+                       '\n': NEWLINE, '<': LESSER,
+                       '>': GREATER, '<>': NOTEQUAL,
+                       '<=': LESSEQUAL, '>=': GREATEREQUAL}
 
         # Dictionary of BASIC reserved words
         keywords = {'LET': LET, 'LIST': LIST, 'PRINT': PRINT,
@@ -72,10 +74,10 @@ class BToken:
             self.category = category  # Category of the token
             self.lexeme = lexeme      # Token in string form
 
-        """
-        Pretty prints the token
-        """
         def pretty_print(self):
+            """Pretty prints the token
+
+            """
             print('Column:', self.column,
                   'Category:', self.category,
                   'Lexeme:', self.lexeme)
