c693056471c18c7602a4a6a7eec1a4c5fed9fb3e
Steve Winslow <steve@swinslow.net>
2020-04-19 13:50:17 -0400

    Add license text and headers
    This adds the GPL-3.0-or-later license text, as well as
    corresponding headers and SPDX identifiers in the individual
    source files, as discussed at:
    https://github.com/richpl/PyBasic/issues/1
    
    It also adds a .gitignore file so that the pycache directory is
    excluded from being added.
    
    Signed-off-by: Steve Winslow <steve@swinslow.net>

--- flowsignal.py
+++ flowsignal.py
@@ -1,5 +1,20 @@
 #! /usr/bin/python
 
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """This class defines an object that can be returned by the BASICParser to
 indicate the need for a control flow branch. The information within the
 object tells the Program the nature of the jump and therefore whether a
